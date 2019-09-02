e193b7cc5716ac20ed97610f0168f0b081838eb0
Moa Lenngren <moa.mail@hotmail.com>
2019-09-02 17:59:19 +0200

    initial commit

---
+++ Assets/PyroManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyroManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] pyros;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        Invoke("PyroOn", 2);
+        Invoke("PyroOff", 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PyroOn()
+    {
+        foreach (GameObject pyro in pyros)
+        {
+            //pyro.gameObject.SetActive(true);
+            pyro.transform.GetChild(0).gameObject.SetActive(true);
+            pyro.transform.GetChild(0).GetComponent<ParticleSystem>().Play();
+            pyro.transform.GetChild(1).GetComponent<ParticleSystem>().Play();
+            pyro.transform.GetChild(2).GetComponent<ParticleSystem>().Play();
+            pyro.transform.GetChild(3).GetComponent<ParticleSystem>().Play();
+            pyro.transform.GetChild(4).GetComponent<ParticleSystem>().Play();
+            pyro.transform.GetChild(5).GetComponent<ParticleSystem>().Play();
+        }
+    }
+
+    public void PyroOff()
+    {
+        foreach (GameObject pyro in pyros)
+        {
+            //pyro.gameObject.SetActive(false);
+            pyro.transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
+            pyro.transform.GetChild(0).gameObject.SetActive(false);
+            pyro.transform.GetChild(1).GetComponent<ParticleSystem>().Stop();
+            pyro.transform.GetChild(2).GetComponent<ParticleSystem>().Stop();
+            pyro.transform.GetChild(3).GetComponent<ParticleSystem>().Stop();
+            pyro.transform.GetChild(4).GetComponent<ParticleSystem>().Stop();
+            pyro.transform.GetChild(5).GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
---
+++ Assets/StrobeLight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrobeLight : MonoBehaviour
+{
+    private bool isOn = false;
+    private Light light;
+    private bool shouldChangeLight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOn && shouldChangeLight)
+        {
+            shouldChangeLight = false;
+            Invoke("TurnOnStrobe", 0.06f);
+        }
+        else if (shouldChangeLight)
+        {
+            Invoke("TurnOffStrobe", 0.06f);
+        }
+    }
+
+    void TurnOnStrobe()
+    {
+        light.enabled = true;
+        isOn = true;
+        shouldChangeLight = true;
+    }
+
+    void TurnOffStrobe()
+    {
+        light.enabled = false;
+        isOn = false;
+        shouldChangeLight = true;
+    }
+}
